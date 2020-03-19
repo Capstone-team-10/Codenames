f396ea9bbeb4cb010ee2324c64b6c277736c2201
AAron Staton <Orson108@hotmail.com>
2020-03-19 19:01:58 -0400

    feature -in App, added route and import for auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//react imports
+import React from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+
+//component imports
+import Navbar from "./components/Navbar";
+import SignInSignUp from "./components/signInSignUpPage/SignInSignUp";
+//style import
+import "./css/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Route path="/auth" component={SignInSignUp} />
+      <Switch>{/* Add other routes here */}</Switch>
+    </Router>
   );
 }
 
