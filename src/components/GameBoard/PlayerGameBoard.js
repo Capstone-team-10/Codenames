bcb229e5ad0fbcabc05ffab4460b03e3b57fe8fb
AAron Staton <Orson108@hotmail.com>
2020-03-21 10:01:12 -0400

    feature -imported PlayArea component, added dealing functionality

--- src/components/GameBoard/PlayerGameBoard.js
+++ src/components/GameBoard/PlayerGameBoard.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SideBar from "./SideBar";
+import PlayArea from "./PlayArea";
 
 import "../../css/playerGameBoard.css";
+import dealCards from "../../utils/dealer";
 
 const PlayerGameBoard = () => {
+  const [deck, setDeck] = useState([]);
+
+  useEffect(() => {
+    console.log("The deck is: ", deck);
+
+    let cardsFromDealer = dealCards();
+    //look into replacing setTimeout with promises
+    setTimeout(() => {
+      setDeck(cardsFromDealer);
+    }, 250);
+  }, []);
+
   return (
     <div>
+      <PlayArea deck={deck} />
       <SideBar />
     </div>
   );
