2e0b7dec5f1d6f5555e53f4e8f41ebbd7811d755
AAron Staton <Orson108@hotmail.com>
2020-03-21 01:08:33 -0400

    feature -added PlayerGameBoard component

---
+++ src/components/GameBoard/PlayerGameBoard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SideBar from "./SideBar";
+
+import "../../css/playerGameBoard.css";
+
+const PlayerGameBoard = () => {
+  return (
+    <div>
+      <SideBar />
+    </div>
+  );
+};
+
+export default PlayerGameBoard;
