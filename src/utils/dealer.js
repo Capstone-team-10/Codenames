1e7206c6e9ff13883560621810f80db3fa17cf3f
AAron Staton <Orson108@hotmail.com>
2020-03-21 16:46:07 -0400

    added -dealer file, deals random words from word list

--- src/utils/dealer.js
+++ src/utils/dealer.js
@@ -1,69 +1,5 @@
 import masterWordList from "./wordBank";
 
-export const buildDeck = wordList => {
-  const cardDeck = [];
-  let red = 9;
-  let blue = 9;
-  let black = 1;
-  let white = 6;
-
-  function getUniqueWord() {
-    const wordSelected = {};
-    let randomWord = wordList[Math.floor(Math.random() * 600)];
-    if (wordSelected[randomWord] === true) {
-      getUniqueWord();
-    }
-    wordSelected[randomWord] = true;
-    return randomWord;
-  }
-
-  // const promiseUniqueWord = new Promise((resolve, reject) => {
-  //   let uniqueWord = null;
-  //   uniqueWord = getUniqueWord();
-  //   if (uniqueWord) {
-  //     resolve(uniqueWord);
-  //   } else {
-  //     reject(new Error("An error occurred while getting a unique word."));
-  //   }
-  // });
-
-  while (red || blue || black || white) {
-    if (red > 0) {
-      cardDeck.push({
-        color: "red",
-        word: `${getUniqueWord()}`,
-        flipped: false
-      });
-      red--;
-    }
-    if (blue > 0) {
-      cardDeck.push({
-        color: "blue",
-        word: `${getUniqueWord()}`,
-        flipped: false
-      });
-      blue--;
-    }
-    if (black > 0) {
-      cardDeck.push({
-        color: "black",
-        word: `${getUniqueWord()}`,
-        flipped: false
-      });
-      black--;
-    }
-    if (white > 0) {
-      cardDeck.push({
-        color: "white",
-        word: `${getUniqueWord()}`,
-        flipped: false
-      });
-      white--;
-    }
-  }
-  return cardDeck;
-};
-
 function shuffle(cardDeck) {
   let currentIndex = cardDeck.length;
   let temporaryValue, randomIndex;
@@ -79,6 +15,75 @@ function shuffle(cardDeck) {
 
   return cardDeck;
 }
+
+//max is the length of the wordList array
+const getRandomNumbers = max => {
+  const numbers = [];
+  for (let i = 0; i < max; i++) {
+    //add index numbers up to wordList length to the array
+    numbers.push(i);
+  }
+  //after the loop numbers is [1,2,3,4,5...up to 600]
+
+  //we shuffle the array of numbers to randomize them
+  const randomNumbers = shuffle(numbers);
+  return randomNumbers.slice(0, 26);
+};
+
+export const buildDeck = wordList => {
+  const randomNumbers = getRandomNumbers(wordList.length);
+  const cardDeck = [];
+  let red = 9;
+  let blue = 9;
+  let black = 1;
+  let white = 6;
+
+  // function getUniqueWord() {
+  //   const wordSelected = {};
+  //   let randomWord = wordList[Math.floor(Math.random() * 600)];
+  //   if (wordSelected[randomWord] === true) {
+  //     getUniqueWord();
+  //   }
+  //   wordSelected[randomWord] = true;
+  //   return randomWord;
+  // }
+
+  while (red || blue || black || white) {
+    if (red > 0) {
+      cardDeck.push({
+        color: "red",
+        word: `${wordList[randomNumbers.shift()]}`,
+        flipped: false
+      });
+      red--;
+    }
+    if (blue > 0) {
+      cardDeck.push({
+        color: "blue",
+        word: `${wordList[randomNumbers.shift()]}`,
+        flipped: false
+      });
+      blue--;
+    }
+    if (black > 0) {
+      cardDeck.push({
+        color: "black",
+        word: `${wordList[randomNumbers.shift()]}`,
+        flipped: false
+      });
+      black--;
+    }
+    if (white > 0) {
+      cardDeck.push({
+        color: "white",
+        word: `${wordList[randomNumbers.shift()]}`,
+        flipped: false
+      });
+      white--;
+    }
+  }
+  return cardDeck;
+};
 
 const dealCards = (wordList = masterWordList) => {
   let deck = buildDeck(wordList);
