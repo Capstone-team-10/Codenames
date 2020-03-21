b34e6ea18ce58e84e478a7fc2df155995f341569
Sunil Abraham <suni.abraham@gmail.com>
2020-03-20 23:48:15 -0400

    add config data

---
+++ fireStore/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDHAJ-kFwWPyokiH9FGotl2yb5gOqQ0Stg",
+  authDomain: "codenames-3a350.firebaseapp.com",
+  databaseURL: "https://codenames-3a350.firebaseio.com",
+  projectId: "codenames-3a350",
+  storageBucket: "codenames-3a350.appspot.com",
+  messagingSenderId: "632107391975",
+  appId: "1:632107391975:web:3779fefbf143543f97bb42",
+  measurementId: "G-Y88T3WM3NM",
+};
+
+export default firebaseConfig;
--- fireStore/index.js
+++ fireStore/index.js
@@ -1,28 +1,7 @@
-const admin = require('firebase-admin');
-// const functions = require('firebase-functions');
+import config from "./config";
+import firebase from "firebase";
+import "firebase/firestore";
 
-// admin.initializeApp(functions.config().firebase);
-
-// let db = admin.firestore();
-
-
-
-// var admin = require("firebase-admin");
-
-// var serviceAccount = require("path/to/serviceAccountKey.json");
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://codenames-3a350.firebaseio.com"
-// });
-
-// Initialize Firebase
-const config = {
-  apiKey: process.env.FIRESTORE_API_KEY
-  authDomain: "codenames-3a350.firebaseapp.com"
-  databaseURL: "https://codenames-3a350.firebaseio.com"
-  projectID: "codenames-3a350"
-  // storageBucket:
-  // messagingSenderId:
-};
 firebase.initializeApp(config);
+
+export default firebase;
