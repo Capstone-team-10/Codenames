1701d615e7032d141378c902f6fc97f86895b545
Stanislav Levitt <stanislavlevitt@yahoo.com>
2020-03-21 12:30:27 -0400

    created ChooseGameRoom component and setup route in the App.js, made comments in Signup component on how to link after onSubmit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import SignInSignUp from "./components/signInSignUpPage/SignInSignUp";
 import Title from "./components/Title";
-
+import ChooseGameRoom from "./components/ChooseGameRoom" // Delete route once form is working
 //style import
 import "./css/App.css";
 
@@ -14,9 +14,12 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Route path="/auth" component={SignInSignUp} />
       <Switch>
-        <Route exact path="/" component={Title}></Route>
+        <Route path="/auth" component={SignInSignUp} />
+        <Route path="/onSubmit" component={ChooseGameRoom}
+        />
+        <Route exact path="/" component={Title}
+        />
       </Switch>
     </Router>
   );
