4d02b4296fbaa9d7f2082eafba337dd14863a67e
Stanislav Levitt <stanislavlevitt@yahoo.com>
2020-03-22 10:25:43 -0400

    1st draft user reducer

--- src/store/user.js
+++ src/store/user.js
@@ -1,49 +1,90 @@
 ///Initial State
 const initialState = {
-  currentuser: {},
+  profile: {},
+  player:{}
 };
 
 ///action
-const GOT_USER = "GOT_USER";
+const GOT_PROFILE = "GOT_PROFILE";
+const SET_PLAYER = "SET_PLAYER";
 
 /// action creator
-const gotUser = user => ({
-  type: GOT_USER,
-  user,
+const gotProfile = profile => ({
+  type: GOT_PROFILE,
+  profile,
+});
+
+const setPlayer = (name, team,master) => ({
+  type: SET_PLAYER,
+  name, team, master,
 });
 
 /// Thunk
-export const getUser = () => async (
-  dispatch,
-  getState,
-  { getFirebase, getFirestore }
+export const getProfile = () => async
+  dispatch
 ) => {
   try {
-    // const {data} = await axios.get(`/api/orders/user/${id}`)
-    // dispatch(gotUser(xxx))
+    db.collection("Users")
+    .where("Name", "==", "AAron")
+    .get()
+    .then(snapshot => {
+      snapshot.docs.forEach(doc => {
+        dispatch(gotProfile(doc.data()))
+      });
+    })
   } catch (error) {
     console.error(error);
   }
 };
 
-// function incrementIfOdd() {
-//   return (dispatch, getState,{getFirebase, getFirestore }) => {
-//     const { counter } = getState();
+export const createProfile = (name,email) => async
+  dispatch
+ => {
+  try {
+    db.collection("Users").add({
+          name: name,
+          email: email
+        }).get()
+        .then(snapshot => {
+          snapshot.docs.forEach(doc => {
+            dispatch(gotProfile(doc.data()))
+          })
+      })
+    }
+  catch (error) {
+    console.error(error);
+  }
+};
 
-//     if (counter % 2 === 0) {
-//       return;
-//     }
+export const settingPlayer = (name,team,master,gamessessionid) => async
+  dispatch
+ => {
+  try {
+    db.collection("Games")
+      .doc(`${gamessessionid}`)
+      .update({
+        UsersInRoom: `${name}`:{
+          displayName: name,
+          Team: team,
+          isSpyMaster: master
+        },
+      });
+    dispatch(setPlayer(name, team,master));
+  }
+  catch (error) {
+    console.error(error);
+  }
+};
 
-//     dispatch(increment());
-//   };
-// }
 
 ///Reducer
 
 export default function(state = initialState, action) {
   switch (action.type) {
-    case GOT_USER:
-      return { ...state, currentuser: action.user };
+    case GOT_PROFILE:
+      return { ...state, profile: action.profile };
+    case SET_PLAYER:
+      return {...state, player: {name:action.name, team:action.team, Spymaster:action.master}}
     default:
       return state;
   }
