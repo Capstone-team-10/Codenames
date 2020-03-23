9ccbed297d9237a10deb1889a2304396f46c7f03
AAron Staton <Orson108@hotmail.com>
2020-03-23 16:16:16 -0400

    update -removed setTimeout and React.Fragments

--- src/components/GameBoard/PlayerGameBoard.js
+++ src/components/GameBoard/PlayerGameBoard.js
@@ -6,31 +6,28 @@ import "../../css/playerGameBoard.css";
 import dealCards from "../../utils/dealer";
 
 const PlayerGameBoard = () => {
-  const spyMaster = false;
+  const spyMaster = true;
   const [deck, setDeck] = useState({ spy: [], spyMaster: [] });
 
   useEffect(() => {
     console.log("The deck is: ", deck);
 
     let cardsFromDealer = dealCards();
-    //look into replacing setTimeout with promises
-    setTimeout(() => {
-      setDeck(cardsFromDealer);
-    }, 250);
+    setDeck(cardsFromDealer);
   }, []);
 
   return (
     <div className="gameBoard-container">
       {spyMaster ? (
-        <React.Fragment>
+        <>
           <PlayArea deck={deck.spyMaster} />
           <SideBar spyMaster={spyMaster} />
-        </React.Fragment>
+        </>
       ) : (
-        <React.Fragment>
+        <>
           <PlayArea deck={deck.spy} />
           <SideBar />
-        </React.Fragment>
+        </>
       )}
     </div>
   );
