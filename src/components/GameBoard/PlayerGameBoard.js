6b76c5620f19ddfef0faa98da0a6c8e3c7b37dad
AAron Staton <Orson108@hotmail.com>
2020-03-25 16:45:24 -0400

    update -created a banned word list and pass it to spymaster SideBar

--- src/components/GameBoard/PlayerGameBoard.js
+++ src/components/GameBoard/PlayerGameBoard.js
@@ -12,6 +12,19 @@ const PlayerGameBoard = ({
   spyMaster,
   teamColor
 }) => {
+  const [bannedWords, setBannedWords] = useState([]);
+
+  useEffect(() => {
+    const banned = [];
+    deck.forEach(({ word, flipped }) => {
+      if (!flipped) {
+        banned.push(word);
+      }
+    });
+    setBannedWords(banned);
+    console.log("banned words are: ", banned);
+  }, [deck]);
+
   return (
     <div className="gameBoard-container">
       {spyMaster ? (
@@ -23,6 +36,7 @@ const PlayerGameBoard = ({
           )}
           <SideBar
             allPlayers={allPlayers}
+            bannedWords={bannedWords}
             displayName={displayName}
             chatLog={chatLog}
             spyMaster={spyMaster}
