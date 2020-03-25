399b1b30896c71217850c8426f5c2e307f2fa11b
Stanislav Levitt <stanislavlevitt@yahoo.com>
2020-03-25 14:06:47 -0400

    Cleaned up code in the Redux Store

--- src/store/turns.js
+++ src/store/turns.js
@@ -1,28 +1,4 @@
 
-
-///Initial State
-const initialState = {
-  currentTeam: "",
-  hint: "",
-  hintCount: 0,
-  GameOver: false,
-};
-
-///action
-const START_GAME = "START_GAME";
-const END_GAME = "END_GAME";
-const END_TURN = "END_TURN";
-const SET_HINT = "SET_HINT";
-
-//  if User.teamColor =/= selectedCard.color || hintCount == 0 Then endTurn Thunk
-/// action creator
-export const endTurn = () => ({ type: END_TURN });
-export const endGame = () => ({ type: END_GAME });
-// Who goes First Logic
-//   Math Random if 0 then blue if 1 then red
-export const startGame = first => ({ type: START_GAME, first });
-export const setHint = (hint, count) => ({ type: SET_HINT, hint, count });
-
 /// Thunk
 export const joinGame = (id,game,user) => async (dispatch, getState, {getFirebase,getFirestore}) => {
   try {
@@ -37,10 +13,6 @@ export const joinGame = (id,game,user) => async (dispatch, getState, {getFirebas
         }
       }
     })
-    // doc(user.uid).set({
-    //   DisplayName: user.displayName
-    // })
-
   }
   catch (error) {
     console.error(error)
@@ -60,8 +32,6 @@ export const newGame = (history,user) => async (dispatch, getState, {getFirebase
       }
     }
    })
-   console.log("Game Ref", id)
-
    history.push(`/play/${id}`)
 
   }
@@ -73,87 +43,13 @@ export const newGame = (history,user) => async (dispatch, getState, {getFirebase
 export const leaveGame = (id,game,user) => async (dispatch, getState, {getFirebase,getFirestore}) => {
   try {
     console.log("Users In room before", game.UsersInRoom)
-     const firestore = getFirestore()
-     const firebase = getFirebase()
-    await firestore.collection("Games").doc(id).update({
-      UsersInRoom: {
-        [user.uid]: firebase.firestore.FieldValue.delete()
-      }
-    })
+    console.log("Specific User", game.UsersInRoom[user.uid])
+    console.log(typeof user.uid)
+    //  const firestore = getFirestore()
+    //  const firebase = getFirebase()
+
   }
   catch (error) {
     console.error(error)
   }
 };
-
-// export const settingHint = (
-//   hintWord,
-//   hintcount,
-//   gamesessionid
-// ) => async dispatch => {
-//   try {
-//     // In the DB change hintword = "" hintcount = 0
-//     db.collection("Games")
-//       .doc(`${gamesessionid}`)
-//       .update({
-//         HintWord: hintWord,
-//         HintCount: hintcount,
-//       });
-
-//     dispatch(setHint(hintWord, hintcount));
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-// export const startingGame = (
-//   gamesessionid,startingTeam
-// ) => async dispatch => {
-//   try {
-//     // In the DB change status of game so id doesn't appear as Open Game in Join Game Lobby
-//     db.collection("Games")
-//       .doc(`${gamesessionid}`)
-//       .update({
-//         status:true
-//       });
-
-//     dispatch(startGame(startingTeam));
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-// export const endingGame = (
-//   gamesessionid
-// ) => async dispatch => {
-//   try {
-//     // Delete game from Game Room ??? How to handle Replay with Same Team Option
-//     db.collection("Games")
-//       .doc(`${gamesessionid}`)
-//       .delete();
-
-//     dispatch(endGame());
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-///Reducer
-
-export default function(state = initialState, action) {
-  switch (action.type) {
-    case END_TURN:
-      return {
-        ...state,
-        currentTeam: state.currentTeam === "red" ? "blue" : "red",
-      };
-    case START_GAME:
-      return { ...state, currentTeam: action.first };
-    case END_GAME:
-      return { ...state, GameOver: true };
-    case SET_HINT:
-      return { ...state, hint: action.hint, hintCount: action.count };
-    default:
-      return state;
-  }
-}
--- src/store/user.js
+++ src/store/user.js
@@ -1,29 +1,6 @@
 import {google} from "../fireStore"
 
-
-///Initial State
-const initialState = {
-  profile: {},
-  player:{}
-};
-
-///action
-const GOT_PROFILE = "GOT_PROFILE";
-const SET_PLAYER = "SET_PLAYER";
-
-/// action creator
-const gotProfile = profile => ({
-  type: GOT_PROFILE,
-  profile,
-});
-
-const setPlayer = (name, team,master) => ({
-  type: SET_PLAYER,
-  name, team, master,
-});
-
 /// Thunk
-
 // Signs user up
 export const createProfile = (name,email,password) => async (dispatch, getState, {getFirebase, getFirestore}) => {
   try {
@@ -75,69 +52,5 @@ export const logout = () => async (dispatch, getState, {getFirebase,getFirestore
   }
   catch (error) {
     console.error(error);
-  }
-};
-
-// export const settingPlayer = (name,team,master,gamessessionid) => async dispatch => {
-//   try {
-//     // db.collection("Games")
-//     //   .doc(`${gamessessionid}`)
-//     //   .update({
-//     //     UsersInRoom: `${name}`:{
-//     //       displayName: name,
-//     //       Team: team,
-//     //       isSpyMaster: master
-//     //     },
-//     //   });
-//     // dispatch(setPlayer(name, team,master));
-//     console.log("SettingPlayer Thunk")
-//   }
-//   catch (error) {
-//     console.error(error);
-//   }
-// };
-
-// export const updateProfile = (id,name,email) => async (dispatch, getState, {getFirebase,getFirestore}) => {
-//   try {
-//     console.log("before", firebase.auth().currentUser)
-//     const firebase = getFirebase()
-//     const user = firebase.auth().currentUser
-//     user.updateProfile({
-//       displayName: name,
-//     })
-//     user.updateEmail(email)
-//     console.log("after", firebase.auth().currentUser)
-//     }
-//   catch (error) {
-//     console.error(error);
-//   }
-// }
-
-
-// export const getProfile = () => async dispatch => {
-//   try {
-//     // db.collection("Users")
-//     // .where("Name", "==", "AAron")
-//     // .get()
-//     // .then(snapshot => {
-//     //   snapshot.docs.forEach(doc => {
-//     //     dispatch(gotProfile(doc.data()))
-//     //   });
-//     // })
-//     console.log(" Get Profile Thunk")
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-///Reducer
-
-export default function(state = initialState, action) {
-  switch (action.type) {
-    case GOT_PROFILE:
-      return { ...state, profile: action.profile };
-    case SET_PLAYER:
-      return {...state, player: {name:action.name, team:action.team, Spymaster:action.master}}
-    default:
-      return state;
   }
 }
