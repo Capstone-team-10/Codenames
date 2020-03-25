2ce73fcdeb6426d2d521c7af0b2e6b7bb016f120
AAron Staton <Orson108@hotmail.com>
2020-03-24 22:30:10 -0400

    feature -in GameLogic added logic for player picks

--- src/components/GameLogic.js
+++ src/components/GameLogic.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import PlayerGameBoard from "./GameBoard";
 
+//import utils
 import { dealCards, dummyData, turnTracker } from "../utils";
-// import dummyData from "../utils/dummyData";
 
 const GameLogic = () => {
   //Dummy data start
@@ -16,9 +16,37 @@ const GameLogic = () => {
   } = dummyData;
   //End dummy data
 
-  const [deck, setDeck] = useState([]);
   const [spyDeck, setSpyDeck] = useState([]);
   const [spyMasterDeck, setSpyMasterDeck] = useState([]);
+  const [pickResult, setPickResult] = useState();
+
+  const cardPick = deck => {
+    return (cardPicked, currentTeam) => {
+      const rightCard = currentTeam;
+      const wrongCard = currentTeam === "red" ? "blue" : "red";
+      const neutralCard = "white";
+      const fatalCard = "black";
+      switch (deck[cardPicked].color) {
+        case rightCard:
+          setPickResult("good");
+          return "good";
+        case neutralCard:
+          setPickResult("neutral");
+          return "neutral";
+        case wrongCard:
+          setPickResult("bad");
+          return "bad";
+        case fatalCard:
+          setPickResult("fatal");
+          return "fatal";
+        default:
+          console.error(
+            `Invalid input cardPicked: ${cardPicked}, currentTeam: ${currentTeam}, deck: `,
+            deck
+          );
+      }
+    };
+  };
 
   const makeSpyAndSpyMasterDecks = deck => {
     const spy = deck.map(({ word, flipped }) => ({ word, flipped }));
@@ -28,9 +56,11 @@ const GameLogic = () => {
   };
 
   useEffect(() => {
-    console.log("The deck is: ", deck);
+    console.log("pickResult is: ", pickResult);
+  }, [pickResult]);
+
+  useEffect(() => {
     let cardsFromDealer = dealCards();
-    setDeck(cardsFromDealer);
     makeSpyAndSpyMasterDecks(cardsFromDealer);
   }, []);
 
@@ -53,6 +83,8 @@ const GameLogic = () => {
           deck={spyDeck}
           displayName={displayName}
           gameStatus={gameStatus}
+          setPickResult={setPickResult}
+          playersPick={cardPick(spyMasterDeck)}
           spyMaster={spyMaster}
           teamColor={teamColor}
         />
