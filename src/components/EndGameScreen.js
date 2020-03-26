a90a6f04c81120c8e759bed12ea4fc37267899a1
Stanislav Levitt <stanislavlevitt@yahoo.com>
2020-03-25 22:09:51 -0400

    Changed Thunk for new Player Button  on EndScreen from DeleteGame Thunk to LeaveGame Thunk

--- src/components/EndGameScreen.js
+++ src/components/EndGameScreen.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import {connect} from "react-redux"
 import { withRouter } from 'react-router'
-import {deleteGame} from "../store/turns"
+import {leaveGame} from "../store/turns"
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 import "../css/EndGameScreen.css";
 const result = "error"
-const gameId = "w6w9HDVm8PzGETWA8Mr4"
+const gameId = "allGIRXkSYuKeOaWNMqY"
 
 const EndGameScreen =(props) => {
   console.log("End Prop", props)
   // const gameId = props.gameId
+
+  const isFetching = props.Games !== undefined
+  const game = isFetching ? props.Games[gameId] : null
 
   let className, message
   switch(result){
@@ -39,9 +44,9 @@ const EndGameScreen =(props) => {
     ///Initiate Same Game Room
   }
 
-  const DeleteGameRoom = (e) =>{
+  const NewGameRoom = (e) =>{
     e.preventDefault()
-    props.deleteGame(gameId)
+    props.LeaveGame(gameId,game,props.User)
     props.history.push("/onSubmit")
   }
 
@@ -53,7 +58,7 @@ const EndGameScreen =(props) => {
       <button className="title-button" onClick={SameGameRoom}>
         Replay with Same Players
       </button>
-      <button className="title-button" onClick={DeleteGameRoom}>
+      <button className="title-button" onClick={NewGameRoom}>
         Replay with New Players
       </button>
       </div>
@@ -61,10 +66,23 @@ const EndGameScreen =(props) => {
   )
 }
 
-const mapDispatchToProps = dispatch => {
+const mapStateToProps = (state) => {
   return {
-    deleteGame: (Gameid) => dispatch(deleteGame(Gameid))
+    Games: state.firestore.data.Games,
+    User: state.firebase.auth,
   }
 }
 
-export default connect(null,mapDispatchToProps)(withRouter(EndGameScreen))
+const mapDispatchToProps = dispatch => {
+  return {
+    LeaveGame: (Gameid,game,user) => dispatch(leaveGame(Gameid,game,user))
+  }
+}
+
+export default compose(
+  firestoreConnect([
+    {
+      collection: "Games"
+    }
+  ]),
+  connect(mapStateToProps,mapDispatchToProps))(withRouter(EndGameScreen))
