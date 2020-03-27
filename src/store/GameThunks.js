482c0a0e9c0e963563aec3e3a324b11ded799285
Sunil Abraham <suni.abraham@gmail.com>
2020-03-26 21:54:07 -0400

    add Chat Thunk

--- src/store/GameThunks.js
+++ src/store/GameThunks.js
@@ -160,3 +160,22 @@ export const GameOver = (id, result) => async (
     return error.message
   }
 };
+
+export const SendMessage = (id, game, user, message) => async (dispatch, getState, { getFirebase, getFirestore }) => {
+  try {
+    const firestore = getFirestore()
+    await firestore.collection("Games").doc(id).set({
+      Chat: [
+        ...game.Chat,
+        {
+          'sender': [user.displayName],
+          'message': message
+        }
+      ]
+    },
+      { merge: true }
+    )
+  } catch (error) {
+    return error.message
+  }
+};
