f2c12ade827f46be2a168c81e9e60ed43fe84039
AAron Staton <Orson108@hotmail.com>
2020-03-31 19:35:43 -0400

    update -added components to the index file

--- src/components/GameBoard/index.js
+++ src/components/GameBoard/index.js
@@ -2,5 +2,8 @@ import PlayerGameBoard from "./PlayerGameBoard";
 export { default as SideBar } from "./SideBar";
 export { default as PlayArea } from "./PlayArea";
 export { default as GameLobby } from "./GameLobby";
+export { default as ConfirmLeave } from "./ConfirmLeave";
+export { default as Notification } from "./Notification";
+export { default as InviteFriendForm } from "./InviteFriendForm";
 
 export default PlayerGameBoard;
