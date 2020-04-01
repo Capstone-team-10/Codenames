d65c03af210f277f35755f7f2a423007fd431731
AAron Staton <Orson108@hotmail.com>
2020-04-01 09:13:44 -0400

    update -Added try catch block to InviteFriend.js

--- src/fireFunctionCalls/inviteFriend.js
+++ src/fireFunctionCalls/inviteFriend.js
@@ -1,13 +1,17 @@
 const inviteFriend = async invite => {
-  const response = await fetch(
-    "http://localhost:5001/codenames-3a350/us-central1/sendInvite",
-    {
-      method: "POST",
-      mode: "no-cors",
-      body: JSON.stringify(invite)
-    }
-  );
-  return await response.json();
+  try {
+    const response = await fetch(
+      "http://localhost:5001/codenames-3a350/us-central1/sendInvite",
+      {
+        method: "POST",
+        mode: "no-cors",
+        body: JSON.stringify(invite)
+      }
+    );
+    return await response.json();
+  } catch (error) {
+    return error;
+  }
 };
 
 export default inviteFriend;
