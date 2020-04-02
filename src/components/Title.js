39e271bba5c2ab4be19a84c56d0e3c124c6aeddc
Stanislav Levitt <stanislavlevitt@yahoo.com>
2020-04-02 13:25:03 -0400

    added isLoggedIn check on title page

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from "react-redux";
 import "../css/Title.css";
 
-export default function Title() {
+const Title = (props) =>{
+  const { isLoggedIn } = props;
   return (
     <div className="title-container">
-      <Link to="/auth/register">
+       {isLoggedIn ? (null) : (<Link to="/auth/register">
       <button className="title-button">
         Want to be a spy!?
       </button>
-      </Link>
+      </Link>)}
     </div>
   )
 }
+
+const mapStateToProps = state => {
+  return {
+    isLoggedIn: !state.firebase.auth.isEmpty
+  };
+};
+
+export default connect(mapStateToProps)(Title);
