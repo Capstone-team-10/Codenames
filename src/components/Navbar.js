53e72a78bf9359f4b3c47d23c7d643611142f47d
AAron Staton <Orson108@hotmail.com>
2020-04-02 13:14:49 -0400

    update -finalized regular styles for Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,79 +28,63 @@ const Navbar = props => {
     }
   };
 
-
   return (
-  <>
-    {isLoggedIn ? (
     <nav id="navbar" className="navbar nav-wrapper red darken-4">
-       <Link to="/onSubmit">
-      <button className="btn controls-btn waves-effect waves-dark teal darken-2">
-       Go to Game Room
-      </button>
-      </Link>
-      <div className="logo-container brand-logo teal darken-2 center">
+      {isLoggedIn ? (
+        <Link to="/onSubmit">
+          <button className="btn controls-btn waves-effect waves-dark">
+            Go to Game Room
+          </button>
+        </Link>
+      ) : (
+        <div></div>
+      )}
+      <div className="logo-container">
         <h1 id="header-logo" className="header-logo hide-on-med-and-down">
-            Codenames
+          Codenames
         </h1>
         <h3
-        id="header-logo"
-        className="header-logo hide-on-large-only hide-on-small-only"
+          id="header-logo"
+          className="header-logo hide-on-large-only hide-on-small-only"
         >
-            Codenames
+          Codenames
         </h3>
-        </div>
+      </div>
+      {isLoggedIn ? (
         <div className="btns-right-container">
           <button
-            className="btn right waves-effect waves-dark teal darken-2"
+            className="controls-btn btn right waves-effect waves-dark"
             onClick={LoggingOut}
           >
             Log Out
           </button>
           <Link to="/userProfile">
-          <button className="btn right waves-effect waves-dark teal darken-2">
-            User Record
-          </button>
+            <button className="controls-btn btn right waves-effect waves-dark">
+              User Record
+            </button>
           </Link>
         </div>
-      </nav>
-    ) : (
-      <nav id="navbar" className="navbar nav-wrapper red darken-4">
-      <div className="logo-container brand-logo teal darken-2 center">
-        <h1 id="header-logo" className="header-logo hide-on-med-and-down">
-          <Link to="/">
-            Codenames
-            </Link>
-        </h1>
-        <h3
-        id="header-logo"
-        className="header-logo hide-on-large-only hide-on-small-only"
-        >
-          <Link to="/">
-            Codenames
-            </Link>
-        </h3>
-        </div>
+      ) : (
         <div className="btns-right-container">
-        <Link to="/auth/register">
-          <button className="btn right waves-effect waves-dark teal darken-2">
-            Sign Up
-          </button>
-        </Link>
+          <Link to="/auth/register">
+            <button className="btn right waves-effect waves-dark teal darken-2">
+              Sign Up
+            </button>
+          </Link>
           <Link to="/auth/login">
             <button className="btn right waves-effect waves-dark teal darken-2">
               Login
             </button>
           </Link>
         </div>
-        </nav>
-    )}
-    </>
-    )
-  }
+      )}
+    </nav>
+  );
+};
 
 const mapStateToProps = state => {
   return {
-    isLoggedIn: !state.firebase.auth.isEmpty,
+    isLoggedIn: !state.firebase.auth.isEmpty
   };
 };
 
