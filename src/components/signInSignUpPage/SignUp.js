0aaceeead750c7bb524a4733d71dbf47d0c35249
Stanislav Levitt <stanislavlevitt@yahoo.com>
2020-04-17 16:53:49 -0400

    fixed edge case, user can not sign up without name field filled out

--- src/components/signInSignUpPage/SignUp.js
+++ src/components/signInSignUpPage/SignUp.js
@@ -65,15 +65,15 @@ const SignUp = (props) => {
         <form id="signUp" onSubmit={submitHandler}>
           <div className="input-field">
             <label htmlFor="name">Display Name</label>
-            <input type="text" id="name" onChange={onChangeHandler} />
+            <input type="text" id="name" onChange={onChangeHandler} required/>
           </div>
           <div className="input-field">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" onChange={onChangeHandler} />
+            <input type="email" id="email" onChange={onChangeHandler} required/>
           </div>
           <div className="input-field">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" onChange={onChangeHandler} />
+            <input type="password" id="password" onChange={onChangeHandler} required/>
           </div>
           <button type="submit" className="btn center ">
             Sign Up
