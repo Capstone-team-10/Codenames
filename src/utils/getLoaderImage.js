64c431606a4d834e913c85e4343063e555f4307d
Astaton <orson108@hotmail.com>
2020-04-21 01:02:55 -0400

    update -keeps track of the last image used to avoid showing same image twice in a row.

--- src/utils/getLoaderImage.js
+++ src/utils/getLoaderImage.js
@@ -1,16 +1,25 @@
-const cardImages = [ 
-    "/images/agent-red-1.jpeg", 
-    "/images/agent-red-2.png", 
-    "/images/agent-blue-1.png", 
-    "/images/agent-blue-2.png", 
-    "/images/neutral-1.png", 
-    "/images/neutral-2.png", 
-    "/images/assassin.jpg"];
+    //getLoaderImage is a function returned by an IIFE.
+    //we are using the IIFEs closure to keep track of the lastImageNo value
+    const getLoaderImage = (() => {
+      const cardImages = [ 
+        "/images/agent-red-1.jpeg", 
+        "/images/agent-red-2.png", 
+        "/images/agent-blue-1.png", 
+        "/images/agent-blue-2.png", 
+        "/images/neutral-1.png", 
+        "/images/neutral-2.png", 
+        "/images/assassin.jpg"];
 
-    export default () => {
-        console.log("In getLoaderResults");
-        return cardImages[
-          Math.floor(Math.random() * cardImages.length)
-        ];
+      let lastImageNo;
+      return () => {
+        let newImageNo = Math.floor(Math.random() * cardImages.length);
+        if(newImageNo === lastImageNo){
+          newImageNo = newImageNo === cardImages.length -1 ? 0 : newImageNo += 1;
+        }
+        lastImageNo = newImageNo;
+        return cardImages[newImageNo];
       };
+    })();
+
+    export default getLoaderImage;
       
